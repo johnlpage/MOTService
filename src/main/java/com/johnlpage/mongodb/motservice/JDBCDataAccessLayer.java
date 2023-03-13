aff58ae312f089dbc53d4516881fd560115bc8aa
John Page <johnlpage@gmail.com>
2023-03-13 16:00:41 +0000

    .

--- src/main/java/com/johnlpage/mongodb/motservice/JDBCDataAccessLayer.java
+++ src/main/java/com/johnlpage/mongodb/motservice/JDBCDataAccessLayer.java
@@ -279,7 +279,7 @@ public class JDBCDataAccessLayer implements MOTDataAccessInterface {
                 JSONObject itemJSON = new JSONObject();
                 for (int col = 1; col <= metaData.getColumnCount(); col++) {
                     String label = metaData.getColumnLabel(col);
-              
+                    logger.info(label);
                     if (firstRow && Arrays.asList(topFieldNames).contains(label.toUpperCase())) {
                         Object val = testResult.getObject(col);
                         jsonObj.put(label.toLowerCase(), val);
