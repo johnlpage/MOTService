ac0eb5cd6648543e9185e7bb59ab4f6aa3db6c34
John Page <johnlpage@gmail.com>
2023-03-15 09:52:23 +0000

    More NULL handling

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,23 @@
 export WRITEHOST="jdbc:postgresql://johnpage-instance-1.c41swlgcxzrp.eu-west-1.rds.amazonaws.com/mot?user=postgres&password=2efdaf4b59" 
 export REPLICAS="jdbc:postgresql://johnpage.cluster-ro-c41swlgcxzrp.eu-west-1.rds.amazonaws.com/mot?user=postgres&password=2efdaf4b59,jdbc:postgresql://ro2.cluster-custom-c41swlgcxzrp.eu-west-1.rds.amazonaws.com/mot?user=postgres&password=2efdaf4b59"
 
+
+export WRITEHOST="jdbc:postgresql://johnpage-instance-1.c41swlgcxzrp.eu-west-1.rds.amazonaws.com/mot?user=postgres&password=2efdaf4b59" 
+export REPLICAS="jdbc:postgresql://johnpage.cluster-ro-c41swlgcxzrp.eu-west-1.rds.amazonaws.com/mot?user=postgres&password=2efdaf4b59,jdbc:postgresql://ro2.cluster-custom-c41swlgcxzrp.eu-west-1.rds.amazonaws.com/mot?user=postgres&password=2efdaf4b59"
+
 mv MOTService.log MOTService.log_`date +%Y%m%d%H%M%S`
+
+
+
+java -jar bin/MOTService.jar -u  $WRITEHOST -r 80 -c 15 -m 5
+java -jar bin/MOTService.jar -u  $WRITEHOST -r 33 -c 33 -m 33
+java -jar bin/MOTService.jar -u  $WRITEHOST -r 100 -c 0 -m 0
+
+java -jar bin/MOTService.jar -u  $WRITEHOST -r 80 -c 15 -m 5 -x $REPLICAS
+java -jar bin/MOTService.jar -u  $WRITEHOST -r 33 -c 33 -m 33 -x $REPLICAS
+java -jar bin/MOTService.jar -u  $WRITEHOST -r 100 -c 0 -m 0 -x $REPLICAS
+
+
 
 java -jar bin/MOTService.jar -u  $WRITEHOST -r 240 -c 45 -m 15
 java -jar bin/MOTService.jar -u  $WRITEHOST -r 100 -c 100 -m 100
